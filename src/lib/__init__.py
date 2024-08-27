92c8a675354cf9e9bfda6b477300394ac83fe359
Dmitry Shneider <dmitryshneider@gmail.com>
2024-08-28 02:30:05 +0600

    Небольшой рефактор логики постов.

--- src/lib/__init__.py
+++
@@ -1,3 +0,0 @@
-from .create_post import *
-
-__all__ = ['create_post']
--- src/posts_handling/send_post.py
+++ src/posts_handling/send_post.py
@@ -1,19 +1,17 @@
 import os
-from aiogram import Router, Bot
+from aiogram import Bot
 from aiogram.types import Message, InputMediaPhoto
 
-from lib.create_post import create_post
+from posts_handling import create_post
 
 dir = '/content/normal'
-post_router = Router()
 
-@post_router.message()
-async def send_post(message: Message, bot: Bot):
+async def send_post(bot: Bot, chat_id: str):
     media = []
     for post in create_post(dir):
         with open(post.image, 'rb') as photo:
             media.append(InputMediaPhoto(photo, caption=post.caption))
-    await bot.send_media_group(chat_id='', media=media)
+    await bot.send_media_group(chat_id=chat_id, media=media)
     media = []
     for post in create_post(dir):
         os.remove(post.image)

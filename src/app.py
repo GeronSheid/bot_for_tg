9157d62f8618d6870ae3786dce168d5dafc35f14
Dmitry Shneider <dmitryshneider@gmail.com>
2024-08-27 23:41:10 +0600

    Добавлена библиотека APScheduler для выполнения действий с определенной переодичностью. Сделал настройки для запуска парсера 1 числа в 00:00 каждого месяца

--- src/app.py
+++ src/app.py
@@ -6,6 +6,9 @@ from dotenv import load_dotenv
 
 
 from database.db import init_db
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.filters import CommandStart
@@ -15,15 +18,24 @@ from aiogram.types import Message
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
+async def run_parser_span():
+    print('запуск парсера')
+
 dp = Dispatcher()
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+scheduller = AsyncIOScheduler()
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer('Ну привет мой маленький любитель поучиться!')
 
+async def on_startup():
+    scheduller.add_job(run_parser_span, 'cron', day=1, hour=0, minute=0)
+    scheduller.start()
+
 async def main() -> None:
     await init_db()
+    await on_startup()
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
